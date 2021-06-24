7525620f42de649fe51822de7c34d7e32146b62c
Evgeny-Chabrovsky <chabor111@gmail.com>
2021-06-24 19:18:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ messege: 'Hello from the server side!', app: 'Natours' });
+// });
+// app.post('/', (req, res) => {
+//   res.send('You can post this endpoin');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+const port = 3003;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
